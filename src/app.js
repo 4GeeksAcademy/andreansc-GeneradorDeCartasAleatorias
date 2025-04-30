12630121bcc7d5d0ed3f81bc85df9649595f0a0b
Andrea Nolasco <ant.andreansc@gmail.com>
2025-04-30 22:21:17 +0000

    Acabe el proyecto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ 
+  const palos = ['♠', '♥', '♦', '♣'];
+  const valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K'];
+
+  const palo = palos[Math.floor(Math.random() * palos.length)];
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+  const color = (palo === '♥' || palo === '♦') ? 'red' : 'black';
+
+  document.getElementById('top-left').innerHTML = `${palo}`;
+  document.getElementById('bottom-right').innerHTML = `${palo}`;
+  document.getElementById('centro').innerText = valor;
+
+
+  document.getElementById('top-left').style.color = color;
+  document.getElementById('bottom-right').style.color = color;
+  
+}
